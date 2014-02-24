4bd8455e254e7906150579fc6a0f41d3a3b8627f
eloone <elodie.rafali@gmail.com>
2014-02-25 00:39:05 +0100

    almost fixed the mess

--- prototype/main.js
+++ prototype/main.js
@@ -1,59 +1,68 @@
 $(document).ready(function(){
 
+	var markerSrc = supportsSVG() ? 'marker.svg' : 'marker.png';
 	var viewportHeight = $(window).height();
-	var m1 = new Marker($('.marker1'));
-	var m2 = new Marker($('.marker2'));
-	var heightToView = Math.max( Math.abs(m2.top() - m1.top()) - viewportHeight, 0);
+	var m1 = new Marker($('<img src="'+markerSrc+'" class="marker marker1"/>'));
+	var m2 = new Marker($('<img src="'+markerSrc+'" class="marker marker2"/>'));
+	var heightToView = getHeightToView(m1, m2);
+	
 	caculateProgress();
 
-	window.onscroll = function(){
-		if(m1.positioned == 'final' && m2.positioned == 'final'){
-			caculateProgress();			
-		}
-	};
-		
+	$(window).on('scroll', function(){
+		caculateProgress();			
+	});
+
+	$(window).on('resize', function(){
+		viewportHeight = $(window).height();
+		caculateProgress();
+	});
+
 	$(document).on('click', function(e){
 		m1.updatePosition(e.pageY);
 		m2.updatePosition(e.pageY);
 	});
 	
 	$(document).on('markerMoved', function(e){
-		heightToView = Math.max( Math.abs(m2.top() - m1.top()) - viewportHeight, 0);
-		if(m1.top()>viewportHeight){
-			heightToView = Math.abs(m2.top() - m1.top());
+		
+		if(m1.top() > m2.top()){
+			var tmp = m2;
+			m2 = m1;
+			m1 = tmp;
 		}
+
+		heightToView = getHeightToView(m1, m2);
+
 		caculateProgress();
 	});
 
 	function caculateProgress(){
+		if(m1.positioned == 'final' && m2.positioned == 'final'){
+			var viewedHeight = viewportHeight - m1.screenTop() - Math.max(0, viewportHeight - m2.screenTop()) ;
 
-		//var viewedHeight = Math.max($(document).scrollTop() - m1.top(), 0);
-		var viewedHeight = viewportHeight - m1.screenTop() ;
-console.log(viewedHeight);
-console.log(heightToView);
-		var progress = (viewedHeight/heightToView)*100;
-	
-		$('.progress').css('width', progress+'%');
+			var progress = (viewedHeight/heightToView)*100;
+		
+			$('.progress').css('width', progress+'%');
+		}
 	}
-	//resize
-});
 
-var markerMoved = new Event('markerMoved');
+});
 
 function Marker(jqueryNode){
 	var $this = this;
 	
 	this.node = jqueryNode;
-	
+
+	this.src = this.node.get(0).src;
+
 	this.node.on('click', function(e){
 		if($this.positioned == 'final'){
-			tooltip = $('<span>Please click on the end position you wish for this marker.</span>');
-			
-			tooltip.css({
-				position:'absolute',
-				left: $this.left() + $this.width() + 5 + 'px',
-				top : $this.top()- 5 + 'px'
-			});
+			$('body').css('cursor', 'url('+$this.src+'), auto');
+
+			if(supportsSVG()){
+				$this.node.get(0).src = 'markerdashed.svg';
+			}
+
+			tooltip = $('<p class="tooltip">Please click on the end position you wish for this marker.</p>');
 	
 			$('body').append(tooltip);
 			
@@ -62,16 +71,21 @@ function Marker(jqueryNode){
 			$this.tooltip = tooltip;
 
 		}
+
 		e.stopPropagation();
 	});
+
+	$('body').append(this.node);
+
 }
 
 Marker.prototype = {
+	eventMoved : new Event("markerMoved"),
 	top : function(){
-		return $(this.node).offset().top;
+		return $(this.node).offset().top + this.fixedHeight()/2;
 	},
 	screenTop : function(){
-		return this.node.get(0).getBoundingClientRect().top;
+		return this.node.get(0).getBoundingClientRect().top + this.fixedHeight()/2;
 	},
 	left : function(){
 		return $(this.node).offset().left;
@@ -81,21 +95,43 @@ Marker.prototype = {
 	},
 	positioned : 'final',
 	moveTo : function(y){
+		
 		this.node.css({
 			left : 0,
 			top : y
 		});
 		
-		document.dispatchEvent(markerMoved);
+		document.dispatchEvent(this.eventMoved);
+	},
+	fixedHeight : function(){
+		if(this.positioned == 'final'){
+			this.height = this.node.height();
+		}
+
+		return this.height;
 	},
 	updatePosition : function(y){
 		if(this.positioned == 'pending'){
 			this.moveTo(y);
+
+			if(supportsSVG()){
+				this.node.get(0).src = 'marker.svg';
+			}
 			
 			this.positioned = 'final';
 			
 			this.tooltip.remove();
+
+			$('body').css('cursor', 'default');
 		}	
 	}			
 };
+
+function getHeightToView(m1, m2){
+	return Math.abs(m2.top() - m1.top());
+}
+
+function supportsSVG() {
+    return !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', "svg").createSVGRect;
+  }
 
