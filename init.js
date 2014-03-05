363fea8cb7099b1d0ba9d802a6d4f53dffd2232c
Elodie Rafalimanana <elodie.rafali@gmail.com>
2014-03-05 10:26:13 +0100

    Delete init.js

--- init.js
+++
@@ -1,9 +0,0 @@
-//reset if click on extension icon
-if(typeof(chromeInfometer) != 'undefined' && chromeInfometer instanceof Extension){
-	chromeInfometer = new Extension();
-};
-
-window.addEventListener('load', function(){
-	chromeInfometer = new Extension();
-});
-
